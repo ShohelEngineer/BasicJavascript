a1b9484546a3e49c89d4e920ad9313241f206bc7
Shohel <shohel.engineer100@gamil.com>
2024-02-08 16:15:49 +0600

    This is variable code

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,5 @@
+// What is Javascript
+//! JavaScript is a powerful programming language that can add interactivity to a website. It was invented by Brendan Eich.
+
+
 console.log("Hello world!");
---
+++ Variable.js
@@ -0,0 +1,7 @@
+//* Variable is a name of storge location.
+
+const firstNumber = 10;
+const secondNumber = 23;
+const result = firstNumber + secondNumber;
+
+console.log(result);
